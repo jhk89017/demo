4180fbef69c077d7df3d4abd00db3358b8fa7fa0
sakura <sakura@DESKTOP-49KJKCB>
2021-07-09 15:19:16 +0900

    헬로 변경

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String hello(Model model) {
 		model.addAttribute("name", "홍길동");
-		
+		//hong이 주석 달았습니다.
 		return "hello";	//=> templates/hello.html 을 찾는 것
 	}
 	
